becef1c0e3f5144d9c52867c0f59f40708acf52a
Sampath <sampath.gonnuru@gmail.com>
2020-06-03 10:26:53 -0400

    invest in particular realestate or not

---
+++ trend_recommendation_1997.sql
@@ -0,0 +1,28 @@
+/*How would you describe the trend in home values for each state from 1997 to 2017? 
+How about from 2007 to 2017? Which states would you recommend for making real estate investments?*/
+
+WITH base as(
+SELECT state, avg(value) as 'BaseYear'
+FROM home_value_data
+WHERE substr(date, 1, 4) = '1997'
+GROUP BY 1),
+
+test as(
+SELECT state, avg(value) as 'TestYear'
+FROM home_value_data
+WHERE substr(date, 1, 4) = '2017'
+GROUP BY 1),
+
+calculation as (
+SELECT base.state, round(((TestYear / BaseYear) * 100) - 100, 2) as 'Trend'
+from base, test
+group by 1)
+
+
+select state, trend,
+CASE
+when trend > 150 then 'Recommend'
+else 'not recommend'
+end as 'verdict'
+from calculation
+GROUP by 1;
